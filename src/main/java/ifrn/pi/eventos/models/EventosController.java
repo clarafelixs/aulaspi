3b4199d5efef628b781c23b404b121c880087993
Maria Clara Félix da Silva <felix.c@academico.ifrn.edu.br>
2022-12-01 11:06:12 -0300

    Atualizando layout da aplicação

--- src/main/java/ifrn/pi/eventos/models/EventosController.java
+++ src/main/java/ifrn/pi/eventos/models/EventosController.java
@@ -1,14 +1,30 @@
 package ifrn.pi.eventos.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ifrn.pi.eventos.repositories.EventoRepository;
+
 @Controller
 public class EventosController {
+	
+	@Autowired
+	private EventoRepository er;
 
 	@RequestMapping("/eventos/form")
 	public String form() {
-		return "formEvento";
+		return "eventos/formEvento";
 		
 	}
+	@RequestMapping("/eventos")
+	public String adicionar(Evento evento) {	
+		
+		System.out.println(evento);
+		er.save(evento);
+		
+		return "eventos/evento-adicionado";
+	}
+	
 }
+ 
